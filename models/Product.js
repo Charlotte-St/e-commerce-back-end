de083ae22fb06d7bf4d6fbb8d0af9a8399883e68
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-15 19:06:40 -0400

    Started adding models and relationships

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,26 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    product_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false},
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
   },
   {
     sequelize,
