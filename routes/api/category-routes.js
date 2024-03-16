85c73dcef3c454ccec774f06c4f7cd9327f94d61
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-16 11:30:53 -0400

    Fixed issue where starting server.js wiped out all seed data

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,15 +3,10 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', async (req, res) => {
-  try {
-    const categoryData = await Category.findAll({
-    include: [{ model: Product }],
-  });
-  res.status(200).json(categoryData);
-  } catch (err){
-    res.status(500).json(err)
-  }
+router.get('/', (req, res) => {
+  Category.findAll().then((categoryData) => {
+    res.status(200).json(categoryData)
+  })
 });
 
 router.get('/:id', async (req, res) => {
