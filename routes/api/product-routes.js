b4ea90abc96cc6f6ac699179fdcb0681d720fc59
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-19 19:51:15 -0400

    Added delete route to product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: ({model: Category, Tag})
+      include: ({model: Category, Tag, ProductTag})
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, Tag}],
+      include: [{ model: Category, Tag, ProductTag}],
     })
     if (!productData) {
       res.status(404).json({ message: 'Product not found.'});
@@ -110,8 +110,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async(req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData){
+      res.status(404).json({ message: 'Product not found. Cannot delete.'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err){
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
