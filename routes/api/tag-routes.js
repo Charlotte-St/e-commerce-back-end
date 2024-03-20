50d7e442ae83b8ffba2c44be73d711f357c72407
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-19 20:26:46 -0400

    Fixing issues with put and delete routes in tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,18 +48,18 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.body, {
+    const tagData = await Tag.update({tag_name: req.body.tag_name},{ returning: true, 
       where: {
-        tag_id: req.params.id,
+        id: req.params.id,
       }
-    });
+  });
     if (!tagData[0]){
       res.status(404).json({ message: 'No tag with this id found.'});
       return;
     }
     res.status(200).json(tagData);
   } catch (err){
-    res.status(500).jason(err)
+    res.status(500).json(err)
   }
 });
 
@@ -68,7 +68,7 @@ router.delete('/:id', async(req, res) => {
   try {
     const tagData = await Tag.destroy({
       where: {
-        tag_id: req.params.id
+        id: req.params.id
       }
     });
     if (!tagData){
